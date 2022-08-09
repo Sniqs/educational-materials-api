644b29d36cf4eaaed96033f364f71c63c229d0fd
Hubert Zycinski <hzycinski@gmail.com>
2022-08-09 14:58:38 +0200

    Remove unneeded using directives

--- EducationalMaterials.Data/DAL/UserRepository.cs
+++ EducationalMaterials.Data/DAL/UserRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EducationalMaterials.Data.DAL
+﻿namespace EducationalMaterials.Data.DAL
 {
     public class UserRepository : Repository<User>, IUserRepository
     {
